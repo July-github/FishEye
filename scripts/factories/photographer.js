13b3c4ad7535b614653565b0c314319926c2dc90
Julie Heckler <july.heckler@gmail.com>
2021-12-29 11:50:59 +0100

    index display

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,28 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price} = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `Sample Photos/Photographers ID Photos/${portrait}`;
+    const rate = price;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const place = document.createElement( 'h3' );
+        const tag = document.createElement( 'span' );
+        const unit = document.createElement( 'p' );
+        place.textContent = city + ', ' + country;
+        tag.textContent = tagline;
+        unit.textContent = rate + '€/jour';
         h2.textContent = name;
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(place);
+        article.appendChild(tag);
+        article.appendChild(unit);
         return (article);
     }
+    
     return { name, picture, getUserCardDOM }
 }
---
+++ scripts/factories/photographer_page.js
@@ -0,0 +1,28 @@
+function photographerPage(data) {
+    const { name, portrait, city, country, tagline} = data;
+
+    const picture = `Sample Photos/Photographers ID Photos/${portrait}`;
+
+    function getUserPageDOM(){
+        const divUser = document.createElement( 'div' );
+        const h2 = document.createElement( 'h2' );
+        const place = document.createElement( 'h3' );
+        const tag = document.createElement( 'span' );
+        place.textContent = city + ', ' + country;
+        tag.textContent = tagline;
+        h2.textContent = name;
+        divUser.appendChild(h2);
+        divUser.appendChild(place);
+        divUser.appendChild(tag);
+        return (divUser);
+    }
+
+    function getUserPictureDOM(){
+        const button = document.getElementsByClassName('contact_button');
+        const divPic = document.createElement( 'img' );
+        divPic.setAttribute("src", picture)
+        button.appendChild(divPic)
+    }
+
+    return { name, picture, getUserPageDOM, getUserPictureDOM }
+}
