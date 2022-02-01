25b4e55bdd36bf9af1135a53b94a26d4950cdb66
Julie Heckler <july.heckler@gmail.com>
2022-02-01 17:08:57 +0100

    code write

--- scripts/factories/picture.js
+++ scripts/factories/picture.js
@@ -7,14 +7,13 @@ export class Media {
         
         if(data.image){
             const media = new Picture(data);
-            const template = new PictureCard(media)
-            
-            return template
+            const template = new PictureCard(media);
+            return template;
         }
         if(data.video){
             const media = new Video(data);
-            const template = new VideoCard(media)
-            return template
+            const template = new VideoCard(media);
+            return template;
         }
         else{
             throw 'media format unknown';
